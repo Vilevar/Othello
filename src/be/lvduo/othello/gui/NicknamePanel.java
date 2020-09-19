0f3c3f53fcd9ce1a7b699da668495d1cc68d7d63
lyqm <maes.lyam@gmail.com>
2020-09-19 22:37:01 +0200

    add style to NicknamePanel

--- src/be/lvduo/othello/gui/NicknamePanel.java
+++ src/be/lvduo/othello/gui/NicknamePanel.java
@@ -3,16 +3,19 @@ package be.lvduo.othello.gui;
 import be.lvduo.othello.Main;
 import be.lvduo.othello.online.Client;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class NicknamePanel implements IGui {
 	
-	private static final double WIDTH = 700, HEIGHT = 450;
+	private static final double WIDTH = 300, HEIGHT = 200;
 	
 	private Stage stage;
 	private Scene scene;
@@ -21,23 +24,40 @@ public class NicknamePanel implements IGui {
 
 	public NicknamePanel() {
 		
+		Text title = new Text("Chose Your Name");
+		title.getStyleClass().add("title");
+		HBox header = new HBox(title);
+		header.setAlignment(Pos.CENTER);
+		
 		name.setPromptText("nickname");
 		name.textProperty().addListener((obs, old, text) -> {
-			if(text.length() < 12)
-				name.setText(text);
+			name.setText(text);
+			if(text.length() > 12 || text.length() < 2)
+				name.getStyleClass().add("not-good");
 			else
-				name.setText(text.substring(0, 12));
+				name.getStyleClass().remove("not-good");
 		});
+		name.getStyleClass().add("not-good");
+		name.getStyleClass().add("textfield");
+		name.setMaxSize(200, 30);
 		
 		Button submit = new Button("Submit");
 		submit.setOnMouseClicked((e) -> {
-			Main.client.setHome(this);
-			Main.client.sendName(name.getText());
+			if(name.getText().length() > 2) {
+				Main.client.setHome(this);
+				Main.client.sendName(name.getText());
+			}
 		});
+		submit.getStyleClass().add("submit-b");
+		
+		VBox container = new VBox(10, name, submit);
+		container.setAlignment(Pos.CENTER);
 		
 		BorderPane pane = new BorderPane();
-		pane.setCenter(new VBox(10, name, submit));
+		pane.setTop(header);
+		pane.setCenter(container);
 		this.scene = new Scene(pane, WIDTH, HEIGHT);
+		this.scene.getStylesheets().add("be/lvduo/othello/gui/css/nickname.css");
 	}
 	
 	public void rep(boolean b) {
@@ -53,6 +73,7 @@ public class NicknamePanel implements IGui {
 		stage.setTitle("Othello - Online");
 		stage.setScene(this.scene);
 		stage.setResizable(false);
+		stage.centerOnScreen();
 		Main.client = new Client("127.0.0.1", 8080);
 		this.stage = stage;
 	}
